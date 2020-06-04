e4190219e3e7c23b202c8d58c4d8051b503c9fbb
Kevin Dong <kevindong80@gmail.com>
2020-06-04 17:32:51 -0400

    Added functionality to retrieve some topic links from a category

--- sandbox/flowster_webscraping/test.py
+++ sandbox/flowster_webscraping/test.py
@@ -3,18 +3,28 @@ import requests
 
 baseURL = 'https://forum.flowster.app'
 
-# Get source HTML text and generate soup object
-sourceHTML = requests.get(baseURL).text
-soup = BeautifulSoup(sourceHTML, 'html.parser')
+# Get base HTML text and generate soup object
+baseHTML = requests.get(baseURL).text
+baseSoup = BeautifulSoup(baseHTML, 'html.parser')
 
-#print(soup.prettify())
+#print(baseSoup.prettify())
 
 # Find all table cells that belong to the category class
-categoryCells = soup.find_all('td', class_='category')
+categoryCells = baseSoup.find_all('td', class_='category')
 
+# Get hyperlink reference and append it to the base URL to get the category page URL
 href = categoryCells[0].div.h3.a['href']
-
 categoryPageURL = baseURL + href
 
 print(categoryPageURL)
+
+# Get category HTML text and generate soup object
+categoryHTML = requests.get(categoryPageURL).text
+categorySoup = BeautifulSoup(categoryHTML, 'html.parser')
+
+topicCells = categorySoup.find_all('td', class_='main-link')
+
+for topic in topicCells:
+    href = topic.a['href']
+    print(href)
 
