a0f605d26d579c0e118b9431d1df4f06cbea97b9
Kevin Dong <kevindong80@gmail.com>
2020-06-05 00:18:45 -0400

    Added functionality to extract topic name and tags

--- sandbox/flowster_webscraping/test.py
+++ sandbox/flowster_webscraping/test.py
@@ -23,7 +23,7 @@ baseSoup = BeautifulSoup(baseHTML, 'html.parser')
 
 #print(baseSoup.prettify())
 
-# Find all table cells that belong to the category class
+# Find all anchor objects that contain category information
 categoryAnchors = baseSoup.find_all('a', class_='category-title-link')
 
 # Get hyperlink reference and append it to the base URL to get the category page URL
@@ -32,7 +32,7 @@ categoryPageURL = baseURL + href
 
 print(categoryPageURL)
 
-# Get category HTML text and generate soup object
+# Get category HTML text
 driver.get(categoryPageURL)
 
 # Load the entire webage by scrolling
@@ -50,15 +50,42 @@ while (True):
         break
     lastHeight = newHeight
 
+# Generate soup object
 categoryHTML = driver.page_source
 categorySoup = BeautifulSoup(categoryHTML, 'html.parser')
 
+# Find all anchor objects that contain topic information
 topicAnchors = categorySoup.find_all('a', class_='title raw-link raw-topic-link')
 
+# Get hyperlink reference and append it to the base URL to get the topic page URL
+href = topicAnchors[1]['href']
+topicPageURL = baseURL + href
+
+# Get category HTML text and generate soup object
+driver.get(topicPageURL)
+topicHTML = driver.page_source
+topicSoup = BeautifulSoup(topicHTML, 'html.parser')
+
+# Get topic name
+topicName = topicSoup.find('a', class_='fancy-title').text
+print(topicName)
+
+# Get topic category and tags
+topicCategoryDiv = topicSoup.find('div', class_='topic-category ember-view')
+
+tagAnchors = topicCategoryDiv.find_all('span', class_='category-name')
+
+for anchor in tagAnchors:
+    print(anchor.text)
+
+
+
+
+'''
 for topic in topicAnchors:
     href = topic['href']
     print(href)
-
+'''
 
 ''' Non-Selenium Implementation
 # Find all table cells that belong to the category class
