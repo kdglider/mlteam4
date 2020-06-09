5ed52c82d34bc63e535e3225426dc189fd7aaea1
Kevin Dong <kevindong80@gmail.com>
2020-06-09 17:54:34 -0400

    Refactored code into a class and added informative comments

--- sandbox/flowster_webscraping/test.py
+++ sandbox/flowster_webscraping/test.py
@@ -1,3 +1,9 @@
+'''
+@file       FlowsterWebscraper.py
+@date       2020/06/09
+@brief      Class to scrape attributes of interest from all topics on the Flowster Discourse forum
+'''
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -8,143 +14,14 @@ import pandas as pd
 import json
 
 
-#####Functions:#####
 '''
-def get_leadc(topicSoup):
-    for c in topicSoup.find_all('meta', property='og:description'):
-        leadc = c.get('content')
-        return leadc
-    
-def get_otherc(topicSoup):
-    postStream = topicSoup.find('div', class_='post-stream')
-    postDivs = postStream.find_all('div', {'class':['topic-post clearfix regular','topic-post clearfix topic-owner regular']})
-    otherc = []
-    for i in range(1, len(postDivs)):
-        post = postDivs[i].find('div', class_='cooked').text
-        otherc.append(post)
-    return otherc
+@brief  Webscraper that scrapes attributes of interest from all topics on the Flowster Discourse forum
 '''
-
-def get_title(topicSoup):
-    topicName = topicSoup.find('a', class_='fancy-title').text
-
-    # Remove leading and trailing spaces and newlines
-    topicName = topicName.replace('\n', '').strip()
-    return topicName
-
-
-def get_category_and_tags(topicSoup):    
-    topicCategoryDiv = topicSoup.find('div', class_='topic-category ember-view')
-    tagAnchors = topicCategoryDiv.find_all('span', class_='category-name')
-
-    tagList = []
-    for anchor in tagAnchors:
-        tagList.append(anchor.text)
-    
-    if (len(tagList) == 1):
-        category = tagList[0]
-        tags = []
-        return category, tags
-    else:
-        category = tagList[0]
-        tags = tagList[1:]
-        return category, tags
-
-    
-def get_author_and_commenters(topicSoup):
-    names = topicSoup.find_all("div", class_="names trigger-user-card")
-    authorList = []
-    for name in names:
-        author = name.span.a.text
-        authorList.append(author)
-    
-    # Remove redundant names
-    authorList = list(set(authorList))
-
-    if (len(authorList) == 1):
-        author = authorList[0]
-        commenters = []
-        return author, commenters
-    else:
-        author = authorList[0]
-        commenters = authorList[1:]
-        return author, commenters
-
-
-def get_comments(topicSoup):
-    postStream = topicSoup.find('div', class_='post-stream')
-    postDivs = postStream.find_all('div', \
-        {'class':['topic-post clearfix regular','topic-post clearfix topic-owner regular']})
-
-    comments = []
-    for i in range(len(postDivs)):
-        comment = postDivs[i].find('div', class_='cooked').text
-        comments.append(comment)
-    
-    if (len(comments) == 1):
-        leadingComment = comments[0]
-        otherComments = []
-        return leadingComment, otherComments
-    else:
-        leadingComment = comments[0]
-        otherComments = comments[1:]
-        return leadingComment, otherComments
-
-
-def get_views(topicSoup):
-    views = topicSoup.find('li', class_='secondary views')
-    if views == None:
-        return str(0)
-    return views.span.text
-    
-
-def get_likes(topicSoup):
-    likes = topicSoup.find('li', class_='secondary likes')
-    if likes == None:
-        return str(0)
-    return likes.span.text
-
-
-
-if __name__=='__main__':
-    # Local path to webdriver
-    webdriverPath = r'C:\Users\kevin\Desktop\chromedriver_win32\chromedriver.exe'
-
-    # Set up webdriver
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')     # Ignore security certificates
-    options.add_argument('--incognito')                     # Use Chrome in Incognito mode
-    options.add_argument('--headless')                      # Run in background
-    driver = webdriver.Chrome( \
-        executable_path = webdriverPath, \
-        options = options)
-
-    # Flowster forum base URL
-    baseURL = 'https://forum.flowster.app'
-
-    # Open Chrome web client using Selenium and retrieve page source
-    driver.get(baseURL)
-    baseHTML = driver.page_source
-
-    # Get base HTML text and generate soup object
-    baseSoup = BeautifulSoup(baseHTML, 'html.parser')
-
-    #print(baseSoup.prettify())
-
-    # Find all anchor objects that contain category information
-    categoryAnchors = baseSoup.find_all('a', class_='category-title-link')
-
-    # Get hyperlink references and append it to the base URL to get the category page URLs
-    categoryPageURLs = []
-    for i in range(len(categoryAnchors)):
-        href = categoryAnchors[i]['href']
-        categoryPageURLs.append(baseURL + href)
-    
-    categoryPageURLs = [categoryPageURLs[0]]
-
-    # Set up both a dictionary and Pandas dataframe to save data to
-    topicDict = {}
-    topicDataframe = pd.DataFrame(columns=[
+class FlowsterWebscraper:
+    driver = None                   # Selenium webdriver object
+    topicDict = {}                  # Dictionary of all topics and their attributes
+    topicDataframe = \
+        pd.DataFrame(columns=[      # Pandas dataframe of all topic attributes
         'Topic Title', 
         'Category', 
         'Tags', 
@@ -155,108 +32,272 @@ if __name__=='__main__':
         'Likes',
         'Views'])
 
-    #1st for_loop ro run through all categories
-    for categoryURL in categoryPageURLs:
-        # Get category HTML text
-        driver.get(categoryURL)
 
-        # Load the entire webage by scrolling
-        lastHeight = driver.execute_script("return document.body.scrollHeight")
-        while (True):
-            # Scroll to bottom of page
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-            # Wait for new page segment to load
-            time.sleep(0.5)
-
-            # Calculate new scroll height and compare with last scroll height
-            newHeight = driver.execute_script("return document.body.scrollHeight")
-            if newHeight == lastHeight:
-                break
-            lastHeight = newHeight
+    def __init__(self, webdriverPath):
+        # Set up webdriver
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')     # Ignore security certificates
+        options.add_argument('--incognito')                     # Use Chrome in Incognito mode
+        options.add_argument('--headless')                      # Run in background
+        self.driver = webdriver.Chrome( \
+            executable_path = webdriverPath, \
+            options = options)
 
 
-        # Generate soup object
-        categoryHTML = driver.page_source
-        categorySoup = BeautifulSoup(categoryHTML, 'html.parser')
+    '''
+    @brief      Retrieves a topic title
+    @param      topicSoup   BeautifulSoup object that contains the topic page HTML
+    @return     topicName   Topic name
+    '''
+    def get_title(self, topicSoup):
+        topicName = topicSoup.find('a', class_='fancy-title').text
 
-        # Find all anchor objects that contain topic information
-        topicAnchors = categorySoup.find_all('a', class_='title raw-link raw-topic-link')
+        # Remove leading and trailing spaces and newlines
+        topicName = topicName.replace('\n', '').strip()
+        return topicName
 
-        # Get hyperlink references and append it to the base URL to get the topic page URLs
+
+    '''
+    @brief      Retrieves a topic's category and tags
+    @param      topicSoup   BeautifulSoup object that contains the topic page HTML
+    @return     category    Category that the topic belongs to
+    @return     tags        List of topic tags
+    '''
+    def get_category_and_tags(self, topicSoup):    
+        topicCategoryDiv = topicSoup.find('div', class_='topic-category ember-view')
+        tagAnchors = topicCategoryDiv.find_all('span', class_='category-name')
+
+        tagList = []
+        for anchor in tagAnchors:
+            tagList.append(anchor.text)
         
-        topicPageURLs = []
-        for i in range(len(topicAnchors)):
-            href = topicAnchors[i]['href']
-            topicPageURLs.append(baseURL + href)
+        if (len(tagList) == 1):
+            category = tagList[0]
+            tags = []
+            return category, tags
+        else:
+            category = tagList[0]
+            tags = tagList[1:]
+            return category, tags
+
+    
+    '''
+    @brief      Retrieves a topic's author and commenters
+    @param      topicSoup   BeautifulSoup object that contains the topic page HTML
+    @return     author      Author username
+    @return     commenters  List of unique commenter usernames
+    '''
+    def get_author_and_commenters(self, topicSoup):
+        names = topicSoup.find_all("div", class_="names trigger-user-card")
+        authorList = []
+        for name in names:
+            author = name.span.a.text
+            authorList.append(author)
+        
+        # Remove redundant names
+        authorList = list(set(authorList))
+
+        if (len(authorList) == 1):
+            author = authorList[0]
+            commenters = []
+            return author, commenters
+        else:
+            author = authorList[0]
+            commenters = authorList[1:]
+            return author, commenters
 
 
-        # 2nd for loop to run through all topics
-        for topicURL in topicPageURLs:
-            # Get category HTML text and generate soup object
-            driver.get(topicURL)
-            topicHTML = driver.page_source
-            topicSoup = BeautifulSoup(topicHTML, 'html.parser')
+    '''
+    @brief      Retrieves a topic's comments
+    @param      topicSoup       BeautifulSoup object that contains the topic page HTML
+    @return     leadingComment  Leading comment (by the author)
+    @return     otherComments   List of other comments
+    '''
+    def get_comments(self, topicSoup):
+        postStream = topicSoup.find('div', class_='post-stream')
+        postDivs = postStream.find_all('div', \
+            {'class':['topic-post clearfix regular','topic-post clearfix topic-owner regular']})
 
-            # Scape all topic attributes of interest
-            topicTitle = get_title(topicSoup)
-            category, tags = get_category_and_tags(topicSoup)
-            author, commenters = get_author_and_commenters(topicSoup)
-            leadingComment, otherComments = get_comments(topicSoup)
-            numLikes = get_likes(topicSoup)
-            numViews = get_views(topicSoup)
+        comments = []
+        for i in range(len(postDivs)):
+            comment = postDivs[i].find('div', class_='cooked').text
+            comments.append(comment)
+        
+        if (len(comments) == 1):
+            leadingComment = comments[0]
+            otherComments = []
+            return leadingComment, otherComments
+        else:
+            leadingComment = comments[0]
+            otherComments = comments[1:]
+            return leadingComment, otherComments
 
-            
-            attributeDict = {
-                'Topic Title'       :   topicTitle,
-                'Category'          :   category,
-                'Tags'              :   tags,
-                'Author'            :   author,
-                'Commenters'        :   commenters,
-                'Leading Comment'   :   leadingComment,
-                'Other Comments'    :   otherComments,
-                'Likes'             :   numLikes,
-                'Views'             :   numViews}
-            
-            topicDict[topicTitle] = attributeDict
 
-            topicDataframe = topicDataframe.append(attributeDict, ignore_index=True)
+    '''
+    @brief      Retrieves a topic's number of views
+    @param      topicSoup           BeautifulSoup object that contains the topic page HTML
+    @return     views.span.text     Number of views as a string
+    '''
+    def get_views(self, topicSoup):
+        views = topicSoup.find('li', class_='secondary views')
+        if views == None:
+            return str(0)
+        return views.span.text
+        
 
-            '''
-            print('Topic Title:')
-            print(topicTitle)
-
-            print('Category:')
-            print(category)
-
-            print('Tags:')
-            print(tags)
-
-            print('Author:')
-            print(author)
-
-            print('Commenters:')
-            print(commenters)
-
-            print('Leading Comment:')
-            print(leadingComment)
-            
-            print('Other Comments:')
-            print(otherComments)
-
-            print('Likes:')
-            print(numLikes)
-
-            print('Views:')
-            print(numViews)
-            '''
+    '''
+    @brief      Retrieves a topic's number of likes
+    @param      topicSoup           BeautifulSoup object that contains the topic page HTML
+    @return     likes.span.text     Number of likes as a string
+    '''
+    def get_likes(self, topicSoup):
+        likes = topicSoup.find('li', class_='secondary likes')
+        if likes == None:
+            return str(0)
+        return likes.span.text
     
 
-    timeStamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    with open('Flowster_Topic_Attributes_' + timeStamp + '.json', 'w') as f:
-        json.dump(topicDict, f)
+    '''
+    @brief      Runs the webscraper application and saves the data in both JSON and CSV files
+    @param      baseURL     Link to the Flowster forum home page
+    @return     None
+    '''
+    def runApplication(self, baseURL):
+        # Open Chrome web client using Selenium and retrieve page source
+        self.driver.get(baseURL)
+        baseHTML = self.driver.page_source
 
-    topicDataframe.to_csv('Flowster_Topic_Attributes_' + timeStamp + '.csv')
+        # Get base HTML text and generate soup object
+        baseSoup = BeautifulSoup(baseHTML, 'html.parser')
+
+        # Find all anchor objects that contain category information
+        categoryAnchors = baseSoup.find_all('a', class_='category-title-link')
+
+        # Get hyperlink references and append it to the base URL to get the category page URLs
+        categoryPageURLs = []
+        for i in range(len(categoryAnchors)):
+            href = categoryAnchors[i]['href']
+            categoryPageURLs.append(baseURL + href)
+
+        # 1st for loop to loop through all categories
+        for categoryURL in categoryPageURLs:
+            # Access category webpage
+            self.driver.get(categoryURL)
+
+            # Load the entire webage by scrolling to the bottom
+            lastHeight = self.driver.execute_script("return document.body.scrollHeight")
+            while (True):
+                # Scroll to bottom of page
+                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+                # Wait for new page segment to load
+                time.sleep(0.5)
+
+                # Calculate new scroll height and compare with last scroll height
+                newHeight = self.driver.execute_script("return document.body.scrollHeight")
+                if newHeight == lastHeight:
+                    break
+                lastHeight = newHeight
+
+            # Generate category soup object
+            categoryHTML = self.driver.page_source
+            categorySoup = BeautifulSoup(categoryHTML, 'html.parser')
+
+            # Find all anchor objects that contain topic information
+            topicAnchors = categorySoup.find_all('a', class_='title raw-link raw-topic-link')
+
+            # Get hyperlink references and append it to the base URL to get the topic page URLs
+            topicPageURLs = []
+            for i in range(len(topicAnchors)):
+                href = topicAnchors[i]['href']
+                topicPageURLs.append(baseURL + href)
+
+
+            # 2nd for loop to loop through all topics in a category
+            for topicURL in topicPageURLs:
+                # Get topic HTML text and generate topic soup object
+                self.driver.get(topicURL)
+                topicHTML = self.driver.page_source
+                topicSoup = BeautifulSoup(topicHTML, 'html.parser')
+
+                # Scape all topic attributes of interest
+                topicTitle = self.get_title(topicSoup)
+                category, tags = self.get_category_and_tags(topicSoup)
+                author, commenters = self.get_author_and_commenters(topicSoup)
+                leadingComment, otherComments = self.get_comments(topicSoup)
+                numLikes = self.get_likes(topicSoup)
+                numViews = self.get_views(topicSoup)
+
+                # Create attribute dictionary for topic
+                attributeDict = {
+                    'Topic Title'       :   topicTitle,
+                    'Category'          :   category,
+                    'Tags'              :   tags,
+                    'Author'            :   author,
+                    'Commenters'        :   commenters,
+                    'Leading Comment'   :   leadingComment,
+                    'Other Comments'    :   otherComments,
+                    'Likes'             :   numLikes,
+                    'Views'             :   numViews}
+                
+                # Add the new entry to the topic dictionary and Pandas dataframe
+                self.topicDict[topicTitle] = attributeDict
+                self.topicDataframe = self.topicDataframe.append(attributeDict, ignore_index=True)
+
+                '''
+                print('Topic Title:')
+                print(topicTitle)
+
+                print('Category:')
+                print(category)
+
+                print('Tags:')
+                print(tags)
+
+                print('Author:')
+                print(author)
+
+                print('Commenters:')
+                print(commenters)
+
+                print('Leading Comment:')
+                print(leadingComment)
+                
+                print('Other Comments:')
+                print(otherComments)
+
+                print('Likes:')
+                print(numLikes)
+
+                print('Views:')
+                print(numViews)
+                '''
+        
+        # Get unique timestamp of the webscraping
+        timeStamp = datetime.now().strftime('%Y%m%d%H%M%S')
+
+        # Save data in JSON and CSV files
+        with open('Flowster_Topic_Attributes_' + timeStamp + '.json', 'w') as f:
+            json.dump(self.topicDict, f)
+
+        self.topicDataframe.to_csv('Flowster_Topic_Attributes_' + timeStamp + '.csv')
+
+
+
+if __name__=='__main__':
+    # Local path to webdriver
+    webdriverPath = r'C:\Users\kevin\Desktop\chromedriver_win32\chromedriver.exe'
+
+    # Flowster forum base URL
+    baseURL = 'https://forum.flowster.app'
+
+    # Create FLowster webscraping object
+    flowsterWebscraper = FlowsterWebscraper(webdriverPath)
+
+    # Run webscraping and save data
+    flowsterWebscraper.runApplication(baseURL)
+
+    
     
     
 
