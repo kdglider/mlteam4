b507568aa934e6a9cdfa12472e57741aa051a70a
Mutaz Ahmed <54813317+mahmed0498@users.noreply.github.com>
2020-06-09 17:05:08 -0400

    Update scrape_comment.py
    Fixed some methods to make the data cleaner when pulled, also adjusted the input so it tries to pull from the csv of links rather than the python script.

--- scrape_comment.py
+++ scrape_comment.py
@@ -4,8 +4,7 @@ import requests
 import pandas as pd
 import csv
 
-
-
+df_post = pd.read_csv("amazon_forum_post_links.csv")
 
 #this method returns the category aka the tag of the webpage
 def get_category():
@@ -13,12 +12,10 @@ def get_category():
     category_name = body.find('span').getText()
     return category_name
 
-
-
 #this method returns a string containing the author of the post
 def get_post_author():
     creators = soup.findAll(class_="creator")
-    post_author = creators[0].find('a').getText()
+    post_author=creators[0].find('a').getText()
     return post_author
 
 #this method returns an array containing a list of the authors of the replies
@@ -34,14 +31,14 @@ def get_reply_authors():
 def get_post_title():
     body = soup.find('body')
     #getting the title
-    post_title = body.find_all('h1')[0].find('a').getText()
+    post_title = body.find_all('h1')[0].find('a').getText().strip()
     return post_title
 
 #this method returns a string of the leading comment of the post
 def get_leading_comment():
     comments = soup.findAll(class_="post")
     #We just need the first comment here
-    leading_comment = comments[0].getText()
+    leading_comment = comments[0].getText().strip()
     return leading_comment
 
 #this method returns an array of strings of the reply comments
@@ -50,14 +47,14 @@ def get_reply_comments():
     comments_arr=[]
     #Look through all the comments except the first which is the leading comment
     for comment in comments[1:]:
-        comments_arr.append(comment.getText())
-    return comments
+      comments_arr.append(comment.getText().strip())
+    return comments_arr
 
 
 #this method returns the time the post was published
 def get_published_time():
     times = soup.findAll("time")
-    publish_time = times[0].getText()
+    publish_time = times[0].getText().strip()
     return publish_time
 
 #this method returns the times for all the replies in an array of type string
@@ -65,7 +62,7 @@ def get_reply_times():
     times = soup.findAll("time")
     reply_times = []
     for time in times[1:]:
-        reply_times.append(time.getText())
+        reply_times.append(time.getText().strip())
     return reply_times
 
 
